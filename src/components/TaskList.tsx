3d225e2b9bff368bc214583bd98ae9823f22c2f1
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2021-03-12 07:30:40 -0300

    :tada: Challenge complete.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,40 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle){
+      return;
+    }
+    
+    const id = (Math.random() * Number.MAX_VALUE) + 1;
+    setTasks([
+      ...tasks,
+      {
+        id,
+        title: newTaskTitle,
+        isComplete: false,
+      }
+    ]);
+
+    setNewTaskTitle('');
   }
 
+  // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          task.isComplete = !task.isComplete;
+        }
+        return task;
+      })
+    );
   }
 
+  // Remova uma task da listagem pelo ID
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
